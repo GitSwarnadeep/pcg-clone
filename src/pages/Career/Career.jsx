bea87dec624445844f105268a2af9de6dbf9c8d0
bikash routh <bikash@prasarnet.team>
2025-08-21 16:16:50 +0530

    add_popup

--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -8,6 +8,9 @@ import ResumeImg from "/assets/career/resume_img.png";
 import OurMission from "/assets/career/our_mission.png";
 import OurVision from "/assets/career/our_vision.png";
 import ProductManagerImg from "/assets/career/product_managerimg.png";
+import ProductManagerImg1 from "/assets/career/product_managerimg1.png";
+import ProductManagerImg2 from "/assets/career/product_managerimg2.png";
+import YoutubeIcon from "/assets/career/youtube_icon.png";
 import GalleryImg1 from "/assets/career/empowered_galleryimg1.png";
 import GalleryImg2 from "/assets/career/empowered_galleryimg2.png";
 import GalleryImg3 from "/assets/career/empowered_galleryimg3.png";
@@ -49,7 +52,7 @@ const Career = () => {
         <div className='container mx-auto px-4 lg:px-24 py-2 flex justify-between items-center'>
             <div className='grid grid-cols-12 gap-4'>
                   <div className='col-span-12 md:col-span-5'>
-                     <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold pt-10 lg:mb-4 text-primary" style={{color:"#1E3A8A"}}>
+                     <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-6xl font-bold pt-10 lg:mb-4 text-primary" style={{color:"#1E3A8A"}}>
                         Build your future with us
                      </h1>
                      <p className='my-6'>
@@ -70,42 +73,42 @@ const Career = () => {
             <div className='grid grid-cols-12 gap-4'>
               <div className='col-span-12 md:col-span-12'>
                   <div className='text-center lg:px-36 mb-16'>
-                     <h2 className='text-white font-bold my-3 text-3xl sm:text-3xl md:text-4xl lg:text-5xl'>Why work at PCIN</h2>
+                     <h2 className='text-white font-bold my-3 text-3xl sm:text-3xl md:text-4xl lg:text-4xl'>Why work at PCIN</h2>
                      <p className='text-white font-normal my-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p>
                   </div>
                   <Slider {...settings}>
                     <div>
                       <div className='bg-white rounded-[10px] p-10 text-center mx-2'>
                         <img src={WhyworkIcon1} className='m-auto'/>
-                        <h3 className='text-black my-3 font-medium text-3xl sm:text-2xl md:text-2xl lg:text-3xl'>Annual Bonus</h3>
+                        <h3 className='text-black my-3 font-semibold text-3xl sm:text-2xl md:text-2xl lg:text-2xl' style={{color:"#3D3D3D"}}>Annual Bonus</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing typesetting.</p>
                       </div>
                     </div>
                     <div>
                       <div className='bg-white rounded-[10px] p-10 text-center mx-2'>
                         <img src={WhyworkIcon2} className='m-auto'/>
-                        <h3 className='text-black my-3 font-medium text-3xl sm:text-2xl md:text-2xl lg:text-3xl'>Annual adjustment</h3>
+                        <h3 className='text-black my-3 font-semibold text-3xl sm:text-2xl md:text-2xl lg:text-2xl' style={{color:"#3D3D3D"}}>Annual adjustment</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing typesetting.</p>
                       </div>
                     </div>
                     <div>
                       <div className='bg-white rounded-[10px] p-10 text-center mx-2'>
                         <img src={WhyworkIcon3} className='m-auto'/>
-                        <h3 className='text-black my-3 font-medium text-3xl sm:text-2xl md:text-2xl lg:text-3xl'>Employee grants</h3>
+                        <h3 className='text-black my-3 font-semibold text-3xl sm:text-2xl md:text-2xl lg:text-2xl' style={{color:"#3D3D3D"}}>Employee grants</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing typesetting.</p>
                       </div>
                     </div>
                     <div>
                       <div className='bg-white rounded-[10px] p-10 text-center mx-2'>
                         <img src={WhyworkIcon1} className='m-auto'/>
-                        <h3 className='text-black my-3 font-medium text-3xl sm:text-2xl md:text-2xl lg:text-3xl'>Annual Bonus</h3>
+                        <h3 className='text-black my-3 font-semibold text-3xl sm:text-2xl md:text-2xl lg:text-3xl' style={{color:"#3D3D3D"}}>Annual Bonus</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing typesetting.</p>
                       </div>
                     </div>
                     <div>
                       <div className='bg-white rounded-[10px] p-10 text-center mx-2'>
                         <img src={WhyworkIcon2} className='m-auto'/>
-                        <h3 className='text-black my-3 font-medium text-3xl sm:text-2xl md:text-2xl lg:text-3xl'>Annual adjustment</h3>
+                        <h3 className='text-black my-3 font-semibold text-3xl sm:text-2xl md:text-2xl lg:text-3xl' style={{color:"#3D3D3D"}}>Annual adjustment</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing typesetting.</p>
                       </div>
                     </div>
@@ -118,7 +121,7 @@ const Career = () => {
         <div className='container mx-auto px-4 lg:px-24 py-2 flex justify-between items-center'>
             <div className='grid grid-cols-12 gap-4'>
                 <div className='col-span-12 md:col-span-12'>
-                  <h2 className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl lg:mb-4 font-bold' style={{color:"#1E3A8A"}}>Open Positions</h2>
+                  <h2 className='text-3xl sm:text-3xl md:text-4xl lg:text-4xl lg:mb-4 font-bold' style={{color:"#1E3A8A"}}>Open Positions</h2>
                 </div>
                 <div className='col-span-12 md:col-span-12'>
                   <Slider {...settings}>
@@ -180,26 +183,26 @@ const Career = () => {
                     <img src={ResumeImg} style={{width:"100%"}}/>
                 </div>  
                 <div className='col-span-12 md:col-span-7 lg:pl-10'>
-                   <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl pr-10 font-bold mb-6'>Didn't Find a Role? Upload Your Resume</h2>
-                   <p className='mb-10'>We're always hiring great talent. Send us your CV.</p>
+                   <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-4xl lg:text-4xl lg:pr-[168px] font-bold mb-6'>Didn't Find a Role? Upload Your Resume</h2>
+                   <p className='mb-8'>We're always hiring great talent. Send us your CV.</p>
                    <form className='grid grid-cols-12 gap-4'>
                         <div className='col-span-12 md:col-span-6'>
                             <label style={{color:"#3D3D3D", width:"100%"}}>Full Name</label>
                             <input type='text' className='mt-3 p-3' placeholder='Enter your full name ' style={{background:"#F8F8F8",border:"1px solid #CECECE",borderRadius:"6px", height:"40px", width:"100%", outline:"none"}}/>
                         </div> 
-                        <div className='col-span-12 md:col-span-6 mb-5'>
+                        <div className='col-span-12 md:col-span-6 mb-3'>
                             <label style={{color:"#3D3D3D", width:"100%"}}>Email Address</label>
                             <input type='text' className='mt-3 p-3' placeholder='Enter your Email ' style={{background:"#F8F8F8",border:"1px solid #CECECE",borderRadius:"6px", height:"40px", width:"100%", outline:"none"}}/>
                         </div> 
                         <div className='col-span-12 md:col-span-12'>
                           <label style={{color:"#3D3D3D", width:"100%"}} className=''>Upload Resume</label>
                           <div className="flex items-center justify-center w-full mt-3">
-                            <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white hover:bg-gray-50">
+                            <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-30 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white hover:bg-gray-50">
                               <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                 <svg className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5-5m0 0l5 5m-5-5v12" />
                                 </svg>
-                                <p className="mb-2 text-sm text-gray-500">
+                                <p className="mb-1 text-sm text-gray-500">
                                   <span className="font-medium text-blue-700">Click to upload</span> or drag and drop
                                 </p>
                                 <p className="text-xs text-gray-400">PDF or DOCX (max. 5MB)</p>
@@ -208,7 +211,7 @@ const Career = () => {
                             </label>
                           </div>
                           <div className='col-span-12 md:col-span-12'>
-                            <button style={{background:"#002F5F", width:"100%", borderRadius:"5px"}} className='p-4 mt-10 text-center text-white'>Send Application</button>
+                            <button style={{background:"#002F5F", width:"100%", borderRadius:"5px"}} className='p-2 mt-10 text-center text-white'>Send Application</button>
                           </div>
                         </div>
                    </form>
@@ -248,38 +251,53 @@ const Career = () => {
         <div className='container mx-auto px-4 lg:px-24 py-2 flex justify-between items-center'>
             <div className='grid grid-cols-12 gap-4'>
                 <div className='col-span-12 md:col-span-12'>
-                  <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-3xl lg:text-5xl pr-10 font-bold mb-5'>People are what matters</h2>
+                  <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-3xl lg:text-4xl pr-10 font-bold mb-5'>People are what matters</h2>
                 </div>
                 <div className='col-span-12 md:col-span-4'>
                   <div style={{border: "1px solid #CECECE", background:"#F8F8F8", borderRadius:"15px"}} className='p-5'>
-                    <div>
+                    <div style={{position:"relative"}}>
                       <img src={ProductManagerImg} alt='img' style={{width:"100%"}}/>
+                      <button command="show-modal" commandfor="dialog">
+                        <div class="absolute top-1/2 left-1/2 w-[60px] -translate-x-1/2 -translate-y-1/2 transform">
+                            <img src={YoutubeIcon} alt='img' style={{width:"100%"}}/>
+                        </div>
+                      </button>
                     </div>
                     <div>
-                       <h3 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-bold pt-10 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
-                       <h5 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
+                       <h3 className='text-2xl sm:text-1xl md:text-2xl lg:text-2xl text-center font-bold pt-0 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
+                       <h5 className='text-2xl sm:text-2xl md:text-2xl lg:text-2xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
                     </div>
                   </div>
                 </div>
                 <div className='col-span-12 md:col-span-4'>
                   <div style={{border: "1px solid #CECECE", background:"#F8F8F8", borderRadius:"15px"}} className='p-5'>
-                    <div>
-                      <img src={ProductManagerImg} alt='img' style={{width:"100%"}}/>
+                    <div style={{position:"relative"}}>
+                      <img src={ProductManagerImg1} alt='img' style={{width:"100%"}}/>
+                      <button command="show-modal" commandfor="dialog">
+                        <div class="absolute top-1/2 left-1/2 w-[60px] -translate-x-1/2 -translate-y-1/2 transform">
+                            <img src={YoutubeIcon} alt='img' style={{width:"100%"}}/>
+                        </div>
+                      </button>
                     </div>
                     <div>
-                       <h3 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-bold pt-10 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
-                       <h5 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
+                       <h3 className='text-2xl sm:text-1xl md:text-2xl lg:text-2xl text-center font-bold pt-0 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
+                       <h5 className='text-2xl sm:text-1xl md:text-2xl lg:text-2xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
                     </div>
                   </div>
                 </div>
                 <div className='col-span-12 md:col-span-4'>
                   <div style={{border: "1px solid #CECECE", background:"#F8F8F8", borderRadius:"15px"}} className='p-5'>
-                    <div>
-                      <img src={ProductManagerImg} alt='img' style={{width:"100%"}}/>
+                    <div style={{position:"relative"}}>
+                      <img src={ProductManagerImg2} alt='img' style={{width:"100%"}}/>
+                      <button command="show-modal" commandfor="dialog">
+                        <div class="absolute top-1/2 left-1/2 w-[60px] -translate-x-1/2 -translate-y-1/2 transform">
+                            <img src={YoutubeIcon} alt='img' style={{width:"100%"}}/>
+                        </div>
+                      </button>
                     </div>
                     <div>
-                       <h3 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-bold pt-10 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
-                       <h5 className='text-2xl sm:text-2xl md:text-2xl lg:text-3xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
+                       <h3 className='text-2xl sm:text-1xl md:text-2xl lg:text-2xl text-center font-bold pt-0 mb-2' style={{color:"#3D3D3D"}}>Jane Doe</h3>
+                       <h5 className='text-2xl sm:text-1xl md:text-2xl lg:text-2xl text-center font-medium' style={{color:"#555555"}}>Product Manager</h5>
                     </div>
                   </div>
                 </div>
@@ -290,7 +308,7 @@ const Career = () => {
         <div className='container mx-auto px-4 lg:px-24 py-2 flex justify-between items-center'>
             <div className='grid grid-cols-12 gap-4'>
                 <div className='col-span-12 md:col-span-12'>
-                  <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl pr-10 font-bold mb-5'>
+                  <h2 style={{color:"#1E3A8A"}} className='text-3xl sm:text-3xl md:text-4xl lg:text-4xl pr-10 font-bold mb-5'>
                     Welcome to Our Empowered Gallery
                   </h2>
                   <p>
@@ -322,6 +340,28 @@ const Career = () => {
         </div>
       </section>                       
       <Footer />
+<el-dialog>
+  <dialog id="dialog" aria-labelledby="dialog-title" class="fixed inset-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent backdrop:bg-transparent">
+    <el-dialog-backdrop class="fixed inset-0 bg-gray-900/50 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
+
+    <div tabindex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+      <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+        <div class="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <iframe width="100%" height="388" src="https://www.youtube.com/embed/NpEaa2P7qZI" title="video placeholder" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          {/* <button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white hover:bg-red-400 sm:ml-3 sm:w-auto">Deactivate</button> */}
+          <button type="button" command="close" commandfor="dialog" class="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto" style={{position:"absolute", top:"10px", background:"#ff00008f", fontSize:"20px"}}>
+            ×
+          </button>
+        </div>
+      </el-dialog-panel>
+    </div>
+  </dialog>
+</el-dialog>
+    
+ 
+
     </>
   )
 }
